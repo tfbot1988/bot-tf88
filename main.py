70b4789bad1914b885f247ef244d5f5ff85324fe
Tien Tran <tranthetien1988@gmail.com>
2026-05-19 13:36:54 +0700

    debug payrollweek sheet names

--- main.py
+++ main.py
@@ -1026,7 +1026,9 @@ async def linkshiftgroup_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE)
 async def payrollweek_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     rate = 30000
     try:
-        sheet = gs_client.open_by_key("1-2CUwuORi7L4H1UmX7n7uUvhMIFXL0_95PVp3_LGGe8").sheet1
+        spreadsheet = gs_client.open_by_key("1-2CUWuORi7L4H1UmX7n7uUvhMIFXL0_95PVp3_LGGe8")
+        print(spreadsheet.worksheets())
+        sheet = spreadsheet.sheet1
         records = sheet.get_all_records()
 
     except Exception as e:
